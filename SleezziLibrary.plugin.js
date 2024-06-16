63ad855cfecaeec0eacac7b37135eca577caf17b
Sleezzi <93204760+Sleezzi@users.noreply.github.com>
2024-06-16 18:20:54 +0200

    Create SleezziLibrary.plugin.js

---
+++ SleezziLibrary.plugin.js
@@ -0,0 +1,23 @@
+/**
+ * @name SleezziLibrary_
+ * @description 
+ * @version 0.0.1
+ * @author Sleezzi
+ * @website https://sleezzi.fr
+*/
+
+const config = {
+
+}
+
+module.exports = class SleezziLibraryPlugin {
+    constructor() {
+        this.config = config;
+    }
+    start() {
+
+    }
+    stop() {
+        
+    }
+}
